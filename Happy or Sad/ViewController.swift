78f95e832a7b3aac55f7cd98056f42bcf677bf7d
WilliamChan <wchan@lcs.on.ca>
2019-11-18 11:16:03 -0500

    Initial Commit

---
+++ Happy or Sad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Happy or Sad
+//
+//  Created by William Chan on 2019-11-18.
+//  Copyright © 2019 WilliamChan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
